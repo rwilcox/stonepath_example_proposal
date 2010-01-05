6c2d67bdf5c961eb9708a1dee097dcdcdaead413
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-01-05 15:47:17 -0500

    documentation on what StonePath gives a Task

--- app/models/evaluate_proposal.rb
+++ app/models/evaluate_proposal.rb
@@ -3,9 +3,14 @@
 # where the reciever will either accept or reject the pointed at proposal
 class EvaluateProposal < ActiveRecord::Base
   include StonePath
-  
+  # This include gives us the following methods in this class now
+  # * a belongs_to :workbench (polymorphic)
+  # * a belongs_to :workitem (polymorphic)
+  # AASM events for #complete, #cancel, #expire
+  # named scopes for unassigned, assigned, active, completed, cancelled, and overdue
+
   stonepath_task
-  
+
   #logs_transitions
   
 end
