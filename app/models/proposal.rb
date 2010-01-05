ae2c412c1026d16d3787ef2520b0e6e502d0dce0
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-01-05 15:48:01 -0500

    define methods for AASM events and add useful finder methods

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -14,10 +14,13 @@ class Proposal < ActiveRecord::Base
   # define the tasks that we need for this model
   # _____________________________________________________________________
   tasked_through :evaluate_proposal   # a proposal can be evaluated by administrative users
+                                      # this gives us a has_many relationship
 
+  task_types = [:evaluate_proposal]   # for the "unassigned_tasks" finder we need this. RPW 01-05-2010
+                                      # This is slightly off the StonePath functionality, but
+                                      # it came up in discussion so it may be useful
 
-
-  # AASM States
+  # AASM States & Events
   # _____________________________________________________________________
   aasm_initial_state :pending
   aasm_state :pending       # nobody has seen this proposal yet, an admin needs to take the first action
@@ -25,5 +28,33 @@ class Proposal < ActiveRecord::Base
   aasm_state :accepted      # this proposal is right for us
   aasm_state :kickback      # The submitter should fix this proposal
 
+  aasm_event :evaluate do
+    transitions :to => :in_evaluation, :from => :pending
+  end
+
+  aasm_eveent :accept do
+    transitions :to => :accepted, :from => :in_evaluation
+  end
+
+
+  # Finder Methods
+  # _____________________________________________________________________
+
+  # Get all "evaluate proposals" StonePath tasks that are unassigned for this entity
+  def unassigned_evaluate_proposals
+    self.evaluate_proposal.unassigned  # use a StonePath provided named scopes
+  end
+
+
+  # Get all unassigned Stone Path tasks for this entity
+  # StonePath doesn't provide this out of the box, so we need to keep track of
+  # our association methods and aggregate them ourselves
+  def unassigned_tasks
+    output = []
+    task_types.each do |association_symbol|
+      output << self.send(association_symbol).unassigned
+    end
+    output
+  end
 
 end
