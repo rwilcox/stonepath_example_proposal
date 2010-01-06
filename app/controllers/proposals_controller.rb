291ff4519274fb562ef54484d4819d1fb529e08c
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-01-06 16:07:57 -0500

    can now add Evaluate Proposal StonePath tasks -- can create and read these tasks

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,9 +1,12 @@
 class ProposalsController < ApplicationController
   before_filter :find_model, :except => [:list, :create]
 
+  # CRUD methods
+  # _____________________________________________________________________
   def index
     @proposals = Proposal.find(:all)
   end
+
 
   def new
     @proposal = Proposal.new
@@ -14,26 +17,46 @@ class ProposalsController < ApplicationController
     @proposals = Proposal.pending
   end
 
+
   def create
     @proposal = Proposal.create(params[:proposal ])
     @proposal.save!
-
-    # TODO: create a task, the Evaluate Proposal
-    
-
     redirect_to :action => "show", :id => @proposal.id
   end
 
+
   def edit
   end
+
 
   def update
     @proposal.update_attributes(params[:proposal])
     redirect_to :action => "show"
   end
 
+
+  def destroy
+    @proposal.destroy
+    redirect_to :action => "index"
+  end
+
+
   def show
   end
+
+
+  # STONE PATH TASK RELATED METHODS
+  # _____________________________________________________________________
+
+  # We need to create the stone path task "Evaluate Proposal" and assign it to a workbench
+  # of an Organization
+  def send_evaluate_prop
+    task = @proposal.evaluate_proposals.create
+    task.workbench = Organization.find params[:organization][:organization_id]
+    task.save
+    redirect_to :action => "pending"
+  end
+
 
   private
   def find_model
--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect 'proposals/pending', :controller => "proposals", :action => "pending"
+  map.connect 'proposals/send_evaluate_prop', :controller => "proposals", :action => "send_evaluate_prop"
   map.resources :proposals
   map.users 'users/:id', :controller => "users"
   #map.connect ':controller/:action/:id'
