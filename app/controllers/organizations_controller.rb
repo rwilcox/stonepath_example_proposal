bbe5ccadf0f0a88a7c2c84265c5a9bf5286dd62f
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-01-06 16:20:18 -0500

    more work on Organization controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,13 @@
 class OrganizationsController < ApplicationController
-  #before_filter :find_model, :except => [:list, :create]
+  before_filter :find_model, :except => [:list, :create]
 
   def index
-    
+    @organizations = Organization.find(:all)
   end
+
+  private
+  def find_model
+    @organization = Organization.find(params[:id]) if params[:id]
+  end
+  
 end
